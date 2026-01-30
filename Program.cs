749854b868401a964322e02ebe1ec53507c28a78
mtlouzada <matheuslouzadaa@gmail.com>
2026-01-29 22:13:44 -0300

    feat: enhance API setup with Swagger and configure database context

--- Program.cs
+++ Program.cs
@@ -1,17 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Teste.Estagio.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("DataSource=Animes.db;Cache=Shared"));
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+app.UseSwagger();
+app.UseSwaggerUI(); 
 
 app.UseHttpsRedirection();
 app.UseRouting();
@@ -22,8 +26,8 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller}/{action=Index}/{id?}"
+);
 
 
 app.Run();
